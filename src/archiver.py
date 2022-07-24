6e8cd09b29dcdd55ed441e529155eb9c4c4a1348
CursedS0uL <32262053+CursedS0uL@users.noreply.github.com>
2022-07-24 21:53:00 +0800

    Updates

--- src/archiver.py
+++ src/archiver.py
@@ -18,14 +18,18 @@ DOM_CHANGES = {}
 APP_PASSWORD = 'happymother123'
 
 
-def web_hash_checker(url, md5, INDEX, json_file):
+def web_hash_checker(url, INDEX):
+    print(url)
+    DRIVER.get(url)
+    ad_blocker()
+    dom = DRIVER.page_source
+    md5 = hashlib.md5(dom.encode("utf-8"))
     digest = md5.hexdigest()
     try:
         if INDEX[url][0].strip('\n') != digest:
             INDEX[url][0] = digest
             print("Website does not match previous hash archive")  # Need user to accept before updating archive
-            page_checker(url)  # function to run if hash is not the same
-            # archive_updater()
+            Diff_url(url)  # function to run if hash is not the same
         else:
             print("Website match previous archive")
             try:
@@ -39,7 +43,6 @@ def web_hash_checker(url, md5, INDEX, json_file):
         INDEX[url][0] = digest
         print("New webpage archived")
         times_url_change_dict[url] = 0
-        # archive_updater(json_file)
 
 
 def format_title(title):
@@ -47,7 +50,7 @@ def format_title(title):
     return title
 
 
-def index_change_history(json_filename):
+def index_change_history(json_filename):  # parsing the change history from the json file into a dict
     with open(json_filename, "r") as file:
         try:
             data = json.load(file)
@@ -59,8 +62,8 @@ def index_change_history(json_filename):
     return times_url_change_dict
 
 
-def update_change_history(json_filename):
-    index_change_history(json_filename)
+def update_change_history():
+    # updating the count in the global variable based on url as the key every time it changes
     # print(times_url_change_dict)
     for key in times_url_change_dict.keys():
         if key in DOM_CHANGES:
@@ -68,46 +71,80 @@ def update_change_history(json_filename):
     # print(times_url_change_dict)
 
 
-def archive_updater(json_filename):
-    # with open("WebHash.txt", "w+") as wf:
-    print("Updating archive")
-    # wf.writelines("\n".join(','.join((key,val)) for (key,val) in INDEX.items()))
-    JSON_values = []  # Archive web page hash
-    temp_dict = {'URLs': {}}
-    update_change_history(json_filename)
-    for key, val in INDEX.items():
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        changes_number = times_url_change_dict[key]
-        JSON_tuple = (key, val[0], now, changes_number)
-        JSON_values.append(JSON_tuple)
-    for val in JSON_values:
-        JSON_dict = json_construct(*val)
-        temp_dict['URLs'].update(JSON_dict)
-    update_json(json_filename, temp_dict)
+def page_source_updater():  # Update page source archive only for urls that changed
     for key in list(DOM_CHANGES.keys()):  # Archive web page code
         # print(DOM_CHANGES.keys())
         DRIVER.get(key)
         page_title = format_title(DRIVER.title)
         old = "archive\\" + page_title + ".html"
         new = "archive\\" + page_title + "_new.html"
-        os.remove(old)
-        os.rename(new, "archive\\" + page_title + ".html")
+        try:
+            if new:
+                os.remove(old)
+                os.rename(new, "archive\\" + page_title + ".html")
+        except:
+            pass
 
 
-def page_archiver(page_source, page_title):
-    page_title = format_title(page_title)
+def json_construct(id, hash, date, times_it_changed):
+    website_dic = {id: {'properties': {}}}
+    values = [{'hash': hash}, {'archival_date': date}, {'number of times URL content change': times_it_changed}]
+    for val in values:
+        website_dic[id]['properties'].update(val)
+    return website_dic
+
+
+def update_json(filename, INDEX):  # updating the json values within the json file and writing out to it
+    JSON_values = []  # Archive web page hash
+    temp_dict = {'URLs': {}}
+    for key, val in INDEX.items():  # using the index global variable
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        changes_number = times_url_change_dict[key]  # using the times_url_change_dict global variable
+        JSON_tuple = (key, val[0], now, changes_number)
+        JSON_values.append(JSON_tuple)
+    for val in JSON_values:
+        JSON_dict = json_construct(*val)
+        temp_dict['URLs'].update(JSON_dict)
+    with open(filename, "r") as rfile: # parsing it previous values, then updating it then write out to file
+        archived_history = json.load(rfile)
+        for key, value in archived_history['URLs'].items():
+            if key in temp_dict['URLs'].keys():
+                archived_history['URLs'][key] = temp_dict['URLs'][key]
+        json_object = json.dumps(archived_history, indent=4)
+        rfile.close()
+    with open(filename, "w") as outfile:
+        outfile.write(json_object)
+
+
+def archive_updater(json_filename): # basically a update all function
+    # with open("WebHash.txt", "w+") as wf:
+    print("Updating archive")
+    # wf.writelines("\n".join(','.join((key,val)) for (key,val) in INDEX.items()))
+    index_change_history(json_filename)
+    update_change_history()
+    update_json(json_filename)
+    page_source_updater()
+
+
+def page_archiver(url):
+    # page archiver to be run at the start to get page source for comparison
+
+    DRIVER.get(url)
+    # need to get code from URL first so that can compare later if there are any changes,
+    # it supports first time archiving also, you can run this on its own to archive html
+    page_source = DRIVER.page_source
+    page_title = format_title(DRIVER.title)
     if not os.path.isfile("archive\\" + page_title + ".html"):
-        #print('First time webpage code archive')
+        print('First time webpage code archive')
         with open("archive\\" + page_title + ".html", "w+") as file:
             file.write(page_source)
     elif os.path.isfile("archive\\" + page_title + ".html"):
-        #print('changed webpage code archived, will use it for comparison
-        # later')
+        print('changed webpage code archived, will use it for comparison later')
         with open("archive\\" + page_title + "_new.html", "w+") as file:
             file.write(page_source)
 
 
-def show_webpage_code_diff(DOM_CHANGES):
+def show_webpage_code_diff():  # basically function  to list before and after changes
     if DOM_CHANGES:  # If there are any changes to any URLs (dictionary not empty)
         print('There are ' + str(len(DOM_CHANGES)) + ' url/s with changes')
         print('The URL/s with changes are:')
@@ -120,51 +157,84 @@ def show_webpage_code_diff(DOM_CHANGES):
             print(value[0])
             print("Changed:")
             print(value[1])
-            print("Removed content:")
-            if (get_removed_content(value[1], value[0])) is False:
-                print("There is no removed content")
-            else:
-                print(get_removed_content(value[1], value[0]))
     else:
         print("there are no changes to any URLs")
 
 
-def Diff(list1, list2):
+def Diff(old_file, new_file):
+    # diff with 2 files as arguments, the two files get from page_archiver, run this only if the hash changed.
+    # this function will return the differences between the two files in a changes_list
+    # element 0 = original content
+    # element 1 = changed content
+    try:
+        f_old = open(old_file)
+        list1 = f_old.readlines()
+        f_new = open(new_file)
+        list2 = f_new.readlines()
+        new_changed_list = []
+        changes_list = []
+        present_in_original_list = list(set(list1) - set(list2))
+        not_present_in_original_list = list(set(list2) - set(list1))
+            # changes_list = list(set(li1) - set(li2)) + list(set(li2) - set(li1))
+
+        for change in present_in_original_list:
+                # arrange by closest match, however may also include stuff that is present in new/old but not in old/new
+
+            new_changed_list.append(" ".join(difflib.get_close_matches(change, not_present_in_original_list, 1)))
+            changes_list.append(present_in_original_list)  # element 0 = original
+            changes_list.append(new_changed_list)   # element 1 = changed
+            return changes_list
+    except Exception as e:
+        print(e)
+
+
+def Diff_url(url):  # same diff function but using url as arguement instead of 2 files
     new_changed_list = []
     changes_list = []
-    present_in_original_list = list(set(list1) - set(list2))
-    not_present_in_original_list = list(set(list2) - set(list1))
-    # changes_list = list(set(li1) - set(li2)) + list(set(li2) - set(li1))
-    for change in present_in_original_list:
-        # arrange by closest match, however may also include stuff that is present in new/old but not in old/new
-        new_changed_list.append(" ".join(difflib.get_close_matches(change, not_present_in_original_list, 1)))
-    # print(previous_list)
-    # print(new_changed_list)
-    changes_list.append(present_in_original_list)
-    changes_list.append(new_changed_list)
-    return changes_list
-
-
-def show_difference(old_file, new_file):
-    f_old = open(old_file)
-    old_text = f_old.readlines()
-    f_new = open(new_file)
-    new_text = f_new.readlines()
-    return Diff(old_text, new_text)
-
-
-def page_checker(url):
     DRIVER.get(url)
     webpage_title = format_title(DRIVER.title)
     old = "archive\\" + webpage_title + ".html"
     new = "archive\\" + webpage_title + "_new.html"
     try:
-        if os.path.isfile(old) and os.path.isfile(new):  # If files exist in the archive
-            DOM_CHANGES[url] = show_difference(old, new)
-        else:
-            print('relevant files does not exist for comparison, could be first time archiving webpage code')
+        if os.path.isfile(old) and os.path.isfile(new):  # If files exist in the archive then compare
+            f_old = open(old)
+            list1 = f_old.readlines()
+            f_new = open(new)
+            list2 = f_new.readlines()
+            present_in_original_list = list(set(list1) - set(list2))
+            not_present_in_original_list = list(set(list2) - set(list1))
+            for change in present_in_original_list:
+                # arrange by closest match, however may also include stuff that is present in new/old but not in old/new
+                new_changed_list.append(" ".join(difflib.get_close_matches(change, not_present_in_original_list, 1)))
+                changes_list.append(present_in_original_list)  # element 0 = original
+                changes_list.append(new_changed_list)  # element 1 = changed
+            DOM_CHANGES[url] = changes_list  # adding to the dom changes
+        # else:
+        #     print('relevant files does not exist for comparison, could be first time archiving webpage code')
     except Exception as e:
         print(e)
+
+#
+# def show_difference(old_file, new_file):
+#     f_old = open(old_file)
+#     old_text = f_old.readlines()
+#     f_new = open(new_file)
+#     new_text = f_new.readlines()
+#     return Diff(old_text, new_text)
+
+
+# def page_checker(url):
+#     DRIVER.get(url)
+#     webpage_title = format_title(DRIVER.title)
+#     old = "archive\\" + webpage_title + ".html"
+#     new = "archive\\" + webpage_title + "_new.html"
+#     try:
+#         if os.path.isfile(old) and os.path.isfile(new):  # If files exist in the archive
+#             DOM_CHANGES[url] = show_difference(old, new)
+#         else:
+#             print('relevant files does not exist for comparison, could be first time archiving webpage code')
+#     except Exception as e:
+#         print(e)
 
 
 def get_removed_content(changed_list, original_list):
@@ -187,7 +257,7 @@ def get_added_content(changed_list, original_list):
         return selected_elements
 
 
-def page_changes_listing(DOM_CHANGES):
+def page_changes_listing():
     show_webpage_code_diff(DOM_CHANGES)
     userinput = input("Do you accept these changes? y/n")  # only when user accepts, then the archive up beu updated
     if userinput.lower() == "y":
@@ -214,7 +284,7 @@ def ad_blocker():
                               """)
 
 
-def report_generation(DOM_CHANGES):
+def report_generation(INDEX):
     if not os.path.isdir("Reports\\"):
         os.mkdir("Reports\\")
     date_time_now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
--- src/parse_json.py
+++ src/parse_json.py
@@ -1,15 +1,13 @@
 import json
 import validator
 
-
 def json_hash_indexer(location="archive/WebHash.Json"):
     with open(location, "r") as file:
         try:
             index = {}
             data = json.load(file)
             for url, properties in data['URLs'].items():
-                index[url] = [properties['properties']['hash'],
-                              properties['properties']['archival_date']]
+                index[url] = [properties['properties']['hash'], properties['properties']['archival_date']]
                 # print(properties)
             return index
         except Exception as e:
@@ -18,28 +16,20 @@ def json_hash_indexer(location="archive/WebHash.Json"):
 
 def json_construct(id, hash, date, times_it_changed):
     website_dic = {id: {'properties': {}}}
-    values = [{'hash': hash}, {'archival_date': date},
-              {'number of times URL content change': times_it_changed}]
+    values = [{'hash': hash}, {'archival_date': date}, {'number of times URL content change': times_it_changed}]
     for val in values:
         website_dic[id]['properties'].update(val)
     return website_dic
 
 
 def update_json(filename, data_dict):
-    with open(filename, "r") as rfile:
-        archived_history = json.load(rfile)
-        for key, value in archived_history['URLs'].items():
-            if key in data_dict['URLs'].keys():
-                archived_history['URLs'][key] = data_dict['URLs'][key]
-        json_object = json.dumps(archived_history, indent=4)
-        rfile.close()
     with open(filename, "w") as outfile:
+        json_object = json.dumps(data_dict, indent=4)
         outfile.write(json_object)
 
 
 def json_verifier(json_filename, decryption_password=123):
-    properties_tuple = (
-    "hash", "archival_date", "number of times URL content change")
+    properties_tuple = ("hash", "archival_date", "number of times URL content change")
     try:
         # json_filename = decrypt_file(json_filename, decryption_password)
         with open(json_filename, "r") as j_file:
@@ -50,21 +40,17 @@ def json_verifier(json_filename, decryption_password=123):
             else:
                 for url, properties in data["URLs"].items():
                     if not validator.is_valid_url(url):
-                        print(
-                            "URLs may be missing or not in correct format, please check")
+                        print("URLs may be missing or not in correct format, please check")
                         return False
                     key, value = list(properties.items())[0]
                     if key != 'properties':
-                        print(
-                            "properties for " + url + " not found, ensure file is in correct format")
+                        print("properties for " + url + " not found, ensure file is in correct format")
                         return False
                     else:
-                        if not {"hash", "archival_date",
-                                "number of times URL content change"} \
+                        if not {"hash", "archival_date", "number of times URL content change"} \
                                <= list(properties.values())[0].keys():
                             print("The missing keys for " + url + " are")
-                            out = [k for k in properties_tuple if
-                                   k not in list(properties.values())[0].keys()]
+                            out = [k for k in properties_tuple if k not in list(properties.values())[0].keys()]
                             print(out)
                             print("Please ensure these properties are present")
                             return False
